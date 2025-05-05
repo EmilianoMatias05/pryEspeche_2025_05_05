2f758f7a97b6cb0a4881c1c151ccae308d0a1275
Alumno <Alumno@PC20>
2025-05-05 11:38:51 -0300

    Agregar archivos de proyecto.

---
+++ pryEspeche_2025_05_05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEspeche_2025_05_05
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmProyectoBase());
+        }
+    }
+}
---
+++ pryEspeche_2025_05_05/frmProyectoBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEspeche_2025_05_05
+{
+    public partial class frmProyectoBase : Form
+    {
+        public frmProyectoBase()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
